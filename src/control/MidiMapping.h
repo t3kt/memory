4fcc587435912aa5f8626e920c89d37c3c80be69
t3kt <tommy.etkin@gmail.com>
2016-07-29 22:47:14 -0700

    extracted out MidiMappingSet into a templated MappingSet<T>

--- src/control/MidiMapping.h
+++ src/control/MidiMapping.h
@@ -12,6 +12,7 @@
 #include <string>
 #include "Common.h"
 #include "JsonIO.h"
+#include "MappingSet.h"
 #include "MidiCommon.h"
 
 class MidiMapping
@@ -38,33 +39,6 @@ private:
   std::string _path;
 };
 
-class MidiMappingSet
-: public JsonReadable
-, public JsonWritable
-, public Outputable {
-public:
-  using Storage = std::vector<MidiMapping>;
-  using iterator = Storage::iterator;
-  using const_iterator = Storage::const_iterator;
-
-  Json to_json() const override;
-  void read_json(const Json& obj) override;
-
-  void add(MidiMapping&& mapping) { _mappings.push_back(mapping); }
-
-  iterator begin() { return _mappings.begin(); }
-  iterator end() { return _mappings.end(); }
-  const_iterator begin() const { return _mappings.begin(); }
-  const_iterator end() const { return _mappings.end(); }
-
-  std::size_t size() const { return _mappings.size(); }
-  bool empty() const { return _mappings.empty(); }
-
-protected:
-  std::string typeName() const override { return "MidiMappingSet"; }
-  void outputFields(std::ostream& os) const override;
-private:
-  Storage _mappings;
-};
+using MidiMappingSet = MappingSet<MidiMapping>;
 
 #endif /* MidiMapping_h */
