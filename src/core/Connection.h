ae6381b17bd512b5a1ab0ea42b028348c4ea9080
t3kt <tommy.etkin@gmail.com>
2016-10-03 20:30:39 -0700

    added EntityConnectionMap

--- src/core/Connection.h
+++ src/core/Connection.h
@@ -9,43 +9,147 @@
 #ifndef Connection_h
 #define Connection_h
 
+#include <unordered_map>
 #include <memory>
 #include "../core/Common.h"
+#include "../core/JsonIO.h"
 #include "../core/WorldObject.h"
 
 class AbstractConnection
-: public NonCopyable {
+: public NonCopyable
+, public JsonWritable {
 public:
-  virtual std::shared_ptr<WorldObject> entityBaseA() = 0;
-  virtual std::shared_ptr<WorldObject> entityBaseB() = 0;
+  virtual std::shared_ptr<WorldObject> entityBase() = 0;
+  virtual bool alive() const = 0;
 };
 
-template<typename Ta, typename Tb>
+template<typename E>
 class EntityConnection
 : public AbstractConnection {
 public:
-  EntityConnection(std::shared_ptr<Ta>& a,
-                   std::shared_ptr<Tb>& b)
-  : _a(a)
-  , _b(b) { }
+  using EntityT = E;
+  using EntityPtr = std::shared_ptr<E>;
 
-  std::shared_ptr<WorldObject> entityBaseA() override {
-    return _a;
+  explicit EntityConnection(EntityPtr& entity)
+  : _entity(entity) { }
+
+  std::shared_ptr<WorldObject> entityBase() override {
+    return _entity;
   }
 
-  std::shared_ptr<WorldObject> entityBaseB() override {
-    return _b;
+  bool alive() const override {
+    return _entity && _entity->alive();
   }
 
-  std::shared_ptr<Ta>& a() { return _a; }
-  const std::shared_ptr<Ta>& a() const { return _a; }
+  EntityPtr& entity() { return _entity; }
+  const EntityPtr& entity() const { return _entity; }
 
-  std::shared_ptr<Tb>& b() { return _b; }
-  const std::shared_ptr<Tb>& b() const { return _b; }
+  Json to_json() const override {
+    return Json::object {
+      {"id", _entity->id()},
+    };
+  }
 
 private:
-  std::shared_ptr<Ta> _a;
-  std::shared_ptr<Tb> _b;
+  EntityPtr _entity;
+};
+
+template<typename TConn>
+class EntityConnectionMap
+: public NonCopyable
+, public JsonWritable {
+public:
+  using ConnT = TConn;
+  using ConnPtr = std::shared_ptr<ConnT>;
+  using EntityPtr = typename ConnT::EntityPtr;
+  using Storage = std::unordered_map<ObjectId, ConnPtr>;
+  using iterator = typename Storage::iterator;
+  using const_iterator = typename Storage::const_iterator;
+
+  bool addConnection(ConnPtr connection) {
+    auto result = _map.insert(std::make_pair(connection->entity()->id(),
+                                             connection));
+    return result.second;
+  }
+
+  template<typename E, typename ...Args>
+  ConnT getOrAdd(std::shared_ptr<E> entity, Args&& ...args) {
+    auto conn = getConnectionTo(entity->id());
+    if (conn) {
+      return conn;
+    }
+    conn = std::make_shared<ConnT>(entity, std::forward<Args>(args)...);
+    addConnection(conn);
+    return conn;
+  }
+
+  ConnPtr getConnectionTo(ObjectId entityId) {
+    auto iter = _map.find(entityId);
+    if (iter == end()) {
+      return nullptr;
+    }
+    return iter->second;
+  }
+
+  EntityPtr getTarget(ObjectId entityId) {
+    auto conn = getConnectionTo(entityId);
+    if (!conn) {
+      return nullptr;
+    }
+    return conn->entity();
+  }
+
+  bool containsId(ObjectId entityId) const {
+    auto iter = _map.find(entityId);
+    return iter != end();
+  }
+
+  void clear() { _map.clear(); }
+
+  std::size_t size() const { return _map.size(); }
+
+  std::size_t erase(ObjectId entityId) { return _map.erase(entityId); }
+
+  iterator begin() { return _map.begin(); }
+  iterator end() { return _map.end(); }
+  const_iterator begin() const { return _map.begin(); }
+  const_iterator end() const { return _map.end(); }
+
+  template<typename A>
+  void performAction(A action) {
+    for (auto& entry : _map) {
+      action(entry->second->entity());
+    }
+  }
+
+  template<typename A>
+  void performConnectionAction(A action) {
+    for (auto& entry : _map) {
+      action(entry->second);
+    }
+  }
+
+  void cullDeadConnections() {
+    for (auto iter = begin();
+         iter != end();) {
+      if (iter->second->alive()) {
+        iter++;
+      } else {
+        iter = _map.erase(iter);
+      }
+    }
+  }
+
+  Json to_json() const override {
+    Json::array arr;
+    for (const auto& entry : _map) {
+      arr.push_back(entry->second->to_json());
+    }
+    return arr;
+  }
+
+private:
+  Storage _map;
 };
 
 #endif /* Connection_h */
--- src/core/WorldObject.h
+++ src/core/WorldObject.h
@@ -12,6 +12,7 @@
 #include <functional>
 #include <iostream>
 #include <memory>
+#include <ofColor.h>
 #include <ofVec3f.h>
 #include "../core/Common.h"
 #include "../core/ObjectId.h"
