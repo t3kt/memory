77c8812a3e01dcd6d1b01ccc1980f40e07e79b01
t3kt <tommy.etkin@gmail.com>
2016-10-06 20:52:15 -0700

    Fixed gui startup slowness using blockLayout().
    see frauzufall/ofxGuiExtended#48

--- src/app/AppGui.cpp
+++ src/app/AppGui.cpp
@@ -30,18 +30,17 @@ void AppGui::setup() {
   _gui = std::make_shared<ofxGui>();
   _gui->setupFlexBoxLayout();
   _mainPanel = _gui->addPanel();
+  _mainPanel->blockLayout(true);
   _mainPanel->setBackgroundColor(ofFloatColor(1, 1, 1, 0.5));
   auto rootGroup = _mainPanel->addGroup();
 
   auto rootTabs = rootGroup->addTabs();
-  rootTabs->setWidth(150);
   rootTabs->setTabHeight(6);
   rootTabs->setTabWidth(44);
   auto coreTab = rootTabs->addGroup(_appParams.core);
   {
     auto entityTabs = rootTabs->addTabs("Ent");
     entityTabs->setTabHeight(6);
-    entityTabs->setTabWidth(44);
     entityTabs->addGroup(_appParams.observers)->setName("Obs");
     entityTabs->addGroup(_appParams.occurrences)->setName("Occ");
     entityTabs->addGroup(_appParams.navigators)->setName("Nav");
@@ -49,7 +48,6 @@ void AppGui::setup() {
   {
     auto visTabs = rootTabs->addTabs("Vis");
     visTabs->setTabHeight(6);
-    visTabs->setTabWidth(44);
     visTabs->addGroup(_appParams.animations)->setName("Ani");
     visTabs->addGroup(_appParams.colors)->setName("Col");
     visTabs->addGroup(_appParams.rendering)->setName("Ren");
@@ -63,7 +61,7 @@ void AppGui::setup() {
   }
 
   loadTheme();
-  _mainPanel->setWidth(150);
+  _mainPanel->blockLayout(false);
 }
 
 void AppGui::addActionButtons(ofxGuiContainer *container) {
