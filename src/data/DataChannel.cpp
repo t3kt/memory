b09874a1e58a47666e9234fa782fbfdb6472c9ab
t3kt <tommy.etkin@gmail.com>
2016-10-18 20:39:56 -0700

    added support for a few more data channel types

--- src/data/DataChannel.cpp
+++ src/data/DataChannel.cpp
@@ -7,7 +7,9 @@
 //
 
 #include <cstring>
+#include <ofConstants.h>
 #include <ofVec3f.h>
+#include <type_traits>
 #include "../data/DataChannel.h"
 
 template<>
@@ -43,6 +45,54 @@ public:
   }
 private:
   float* _data;
+};
+
+template<typename T>
+class StaticCastDataOutputChannel
+: public AbstractDataOutputChannel {
+public:
+  StaticCastDataOutputChannel(std::string name)
+  : AbstractDataOutputChannel(name)
+  , _channel(name) { }
+
+  void bind(float* data, std::size_t size) {
+    _channel.bind(data, size);
+  }
+
+  void unbind() {
+    _channel.unbind();
+  }
+
+  bool ready() const override {
+    return _channel.ready();
+  }
+
+  void wipe() override {
+    _channel.wipe();
+  }
+
+  void setSlice(std::size_t index, const T& value) {
+    _channel.setSlice(index, static_cast<float>(value));
+  }
+
+private:
+  DataOutputChannel<float> _channel;
+};
+
+template<>
+class DataOutputChannel<int>
+: public StaticCastDataOutputChannel<int> {
+public:
+  DataOutputChannel(std::string name)
+  : StaticCastDataOutputChannel(name) { }
+};
+
+template<>
+class DataOutputChannel<bool>
+: public StaticCastDataOutputChannel<bool> {
+public:
+  DataOutputChannel(std::string name)
+  : StaticCastDataOutputChannel(name) { }
 };
 
 template<>
@@ -88,4 +138,56 @@ private:
   DataOutputChannel<float> _xChannel;
   DataOutputChannel<float> _yChannel;
   DataOutputChannel<float> _zChannel;
+};
+
+template<>
+class DataOutputChannel<ofFloatColor>
+: public AbstractDataOutputChannel {
+public:
+  DataOutputChannel(std::string name,
+                    std::string rSuffix = "r",
+                    std::string gSuffix = "g",
+                    std::string bSuffix = "b",
+                    std::string aSuffix = "a")
+  : AbstractDataOutputChannel(name)
+  , _rChannel(name + rSuffix)
+  , _gChannel(name + gSuffix)
+  , _bChannel(name + bSuffix)
+  , _aChannel(name + aSuffix) { }
+
+  void bind(float* rData,
+            float* gData,
+            float* bData,
+            float* aData,
+            std::size_t size) {
+    _rChannel.bind(rData, size);
+    _gChannel.bind(gData, size);
+    _bChannel.bind(bData, size);
+    _aChannel.bind(aData, size);
+    _size = size;
+  }
+
+  void unbind() {
+    bind(nullptr,
+         nullptr,
+         nullptr,
+         nullptr,
+         0);
+  }
+
+  bool ready() const override {
+    return _rChannel.ready() && _gChannel.ready() && _bChannel.ready() && _aChannel.ready() && _size > 0;
+  }
+
+  void wipe() override {
+    _rChannel.wipe();
+    _gChannel.wipe();
+    _bChannel.wipe();
+    _aChannel.wipe();
+  }
+private:
+  DataOutputChannel<float> _rChannel;
+  DataOutputChannel<float> _gChannel;
+  DataOutputChannel<float> _bChannel;
+  DataOutputChannel<float> _aChannel;
 };
